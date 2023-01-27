f3969d354b3b4bb9297bfe2c759d279655324741
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-27 14:05:58 +0100

    chore:added the links and style

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,10 +1,49 @@
 import React, { useState, useRef, useEffect } from "react";
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
 import { FaBars, FaTwitter } from "react-icons/fa";
-import { links, social } from "./data";
-import logo from "./logo.svg";
+import { links, social } from "./Navdata";
+
+// import logo from "./logo.svg";
+import Gallery from "./../pages/gallery";
 
 const Navbar = () => {
-  return <h4>navbar</h4>;
+  return (
+    <>
+      <nav>
+        <Box className="nav-center">
+          <Box className="nav-header">
+            {/* <Image src={logo} alt="logo" /> */}
+            <h1>Logo</h1>
+            <button className="nav-toggle">
+              <FaBars />
+            </button>
+          </Box>
+          <Box className="links-container show-container">
+            <li className="links">
+              <a href="">Home</a>
+            </li>
+            <li className="links">
+              <a href="">About-us</a>
+            </li>
+            <li className="links">
+              <a href="">Facilities</a>
+            </li>
+            <li className="links">
+              <a href="">Gallery</a>
+            </li>
+            <li className="links">
+              <a href="">Contact</a>
+            </li>
+            <li className="links">
+              <a href="">Faq</a>
+            </li>
+          </Box>
+          <ul className="social-icons"></ul>
+        </Box>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
