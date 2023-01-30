557bfaed56182fe99fda7bc9738501c7c360f464
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-31 00:31:56 +0100

    chore:added images to the project

--- Components/Contact/Contact.js
+++ Components/Contact/Contact.js
@@ -5,16 +5,18 @@ const Contact = () => {
   return (
     <>
       <Box>
-        <Box className="contact-contact faq-container">
-          <h3>CONTACT US</h3>
-          <h1>We are here because of you.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-            aspernatur voluptatem commodi quis. Dolorem nulla officiis quae
-            ullam? Mollitia cumque odio, laborum in molestias itaque sed
-            deserunt illum accusantium blanditiis dolore adipisci perferendis
-            atque quia eum reiciendis hic quidem perspiciatis.
-          </p>
+        <Box className="contact-container">
+          <div className="contact-text">
+            <h3>CONTACT US</h3>
+            <h1>We are here because of you.</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              aspernatur voluptatem commodi quis. Dolorem nulla officiis quae
+              ullam? Mollitia cumque odio, laborum in molestias itaque sed
+              deserunt illum accusantium blanditiis dolore adipisci perferendis
+              atque quia eum reiciendis hic quidem perspiciatis.
+            </p>
+          </div>
         </Box>
       </Box>
     </>
