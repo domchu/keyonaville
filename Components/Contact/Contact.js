ad5554ef52a8816f03c7f47565ae2e25bf1a66d9
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-30 23:11:11 +0100

    text:added text to the contact

--- Components/Contact/Contact.js
+++ Components/Contact/Contact.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>contact</h1>
-    </div>
+    <>
+      <Box>
+        <Box className="contact-contact faq-container">
+          <h3>CONTACT US</h3>
+          <h1>We are here because of you.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            aspernatur voluptatem commodi quis. Dolorem nulla officiis quae
+            ullam? Mollitia cumque odio, laborum in molestias itaque sed
+            deserunt illum accusantium blanditiis dolore adipisci perferendis
+            atque quia eum reiciendis hic quidem perspiciatis.
+          </p>
+        </Box>
+      </Box>
+    </>
   );
 };
 
