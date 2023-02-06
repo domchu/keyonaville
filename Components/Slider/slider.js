26e7f5b8854b018811a66902f46419207f1d93ad
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-06 23:53:56 +0100

    chore:install the react slick css

--- Components/Slider/slider.js
+++ Components/Slider/slider.js
@@ -1,29 +1,31 @@
-import React, { Components } from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default class Responsive extends Component {
   render() {
     var settings = {
+      //   dots: true,
+      //   infinite: true,
+      //   slidesToShow: 1,
+      //   slidesToScroll: 1,
+      //   initialSlide: 1,
+      autoplay: true,
+      speed: 500,
+      autoplaySpeed: 500,
+      cssEase: "linear",
       dots: true,
       infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      autoplay: true,
-      speed: 2000,
-      autoplaySpeed: 2000,
-      cssEase: "linear",
-      // dots: true,
-      // infinite: true,
-      // speed: 500,
-      // slidesToShow: 1,
-      // slidesToScroll: 1
+      //   speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             infinite: true,
             dots: true,
           },
@@ -31,9 +33,9 @@ export default class Responsive extends Component {
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1,
           },
         },
         {
