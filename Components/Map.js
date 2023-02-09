1ef00eeb0cf3d50a387a18e1495124a8de9e5a3a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-09 17:18:08 +0100

    chore:input the open days counter

--- Components/Map.js
+++ Components/Map.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Flex, Box } from "@chakra-ui/react";
+import openDays from "./openDays";
 // import OpenDays from "./OpenDays";
 
 const Map = () => {
@@ -8,6 +9,7 @@ const Map = () => {
   function activeDays() {
     let today = new Date();
     let currentDay = today.getDate();
+    console.log(currentDay);
     setGetDays(currentDay);
   }
   useEffect(() => {
@@ -15,69 +17,85 @@ const Map = () => {
   }, []);
   return (
     <>
-      <Flex
-        flexDirection={{ base: "column", m: "column", md: "row", lg: "row" }}
-        className="wrapper-contact"
-      >
-        <Box className="banner" m={{ base: 20, sm: 20, md: 0, lg: 0 }}>
-          <ul>
-            <li>
-              <Flex
-                justifyContent="space-around"
-                paddingTop="2rem"
-                paddingBottom="2rem"
-                color="green.300"
-                fontWeight="600"
-              >
-                <span>Open Day</span>
-                <span>Open Hour</span>
-              </Flex>
-            </li>
-            <li className={getDays == 1 ? "active" : "days"}>
-              <Flex justifyContent="space-around" paddingBottom="10px">
-                <span>Monday</span>
-                <span>7am - 5:30pm</span>
-              </Flex>
-            </li>
-            <li className={getDays == 1 ? "active" : "days"}>
-              <Flex justifyContent="space-around" paddingBottom="10px">
-                <span>Tuesday</span>
-                <span>7am - 5:30pm</span>
-              </Flex>
-            </li>
-            <li className={getDays == 1 ? "active" : "days"}>
-              <Flex justifyContent="space-around" paddingBottom="10px">
-                <span>Wednesday</span>
-                <span>7am - 5:30pm</span>
-              </Flex>
-            </li>
-            <li className={getDays == 1 ? "active" : "days"}>
-              <Flex justifyContent="space-around" paddingBottom="10px">
-                <span>Thursday</span>
-                <span>7am - 5:30pm</span>
-              </Flex>
-            </li>
-            <li className={getDays == 1 ? "active" : "days"}>
-              <Flex justifyContent="space-around" paddingBottom="10px">
-                <span>Friday</span>
-                <span>7am - 5:30pm</span>
-              </Flex>
-            </li>
-          </ul>
-        </Box>
-        <Box className="banner-location" m={{ base: 20, sm: 20, md: 0, lg: 0 }}>
-          <Box className="frame-wrapper"></Box>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7928.753336234345!2d3.1731800210065506!3d6.4738819333213105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b819c6fba15b1%3A0xacc3dbc2fa9d737a!2sKeyonavilleschool!5e0!3m2!1sen!2sng!4v1675896538770!5m2!1sen!2sng"
-            width="600"
-            height="450"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </Box>
-        {/* <openDays /> */}
-      </Flex>
+      <Box>
+        <Flex
+          flexDirection={{ base: "column", m: "column", md: "row", lg: "row" }}
+          className="wrapper-contact"
+        >
+          <Box className="banner" m={{ base: 20, sm: 20, md: 0, lg: 0 }}>
+            <ul>
+              <li>
+                <Flex
+                  justifyContent="space-around"
+                  paddingTop="2rem"
+                  paddingBottom="2rem"
+                  color="green.300"
+                  fontWeight="600"
+                >
+                  <span>Open Day</span>
+                  <span>Open Hour</span>
+                </Flex>
+              </li>
+              <li className={getDays == 0 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Sunday</span>
+                  <span>closed</span>
+                </Flex>
+              </li>
+              <li className={getDays == 1 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Monday</span>
+                  <span>7am - 5:30pm</span>
+                </Flex>
+              </li>
+              <li className={getDays == 2 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Tuesday</span>
+                  <span>7am - 5:30pm</span>
+                </Flex>
+              </li>
+              <li className={getDays == 3 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Wednesday</span>
+                  <span>7am - 5:30pm</span>
+                </Flex>
+              </li>
+              <li className={getDays == 4 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Thursday</span>
+                  <span>7am - 5:30pm</span>
+                </Flex>
+              </li>
+              <li className={getDays == 5 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Friday</span>
+                  <span>7am - 5:30pm</span>
+                </Flex>
+              </li>
+              <li className={getDays == 6 ? "active" : "days"}>
+                <Flex justifyContent="space-around" paddingBottom="10px">
+                  <span>Saturday</span>
+                  <span>closed</span>
+                </Flex>
+              </li>
+            </ul>
+          </Box>
+          <Box
+            className="banner-location"
+            m={{ base: 20, sm: 20, md: 0, lg: 0 }}
+          >
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7928.753336234345!2d3.1731800210065506!3d6.4738819333213105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b819c6fba15b1%3A0xacc3dbc2fa9d737a!2sKeyonavilleschool!5e0!3m2!1sen!2sng!4v1675896538770!5m2!1sen!2sng"
+              width="600"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
+          {/* <openDays /> */}
+        </Flex>
+      </Box>
     </>
   );
 };
