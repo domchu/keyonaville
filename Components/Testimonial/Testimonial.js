6e6de38e1549b5c157bea4db47dcb8b270709fd5
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-11 02:46:18 +0100

    chore:done with the testimonial slider

--- Components/Testimonial/Testimonial.js
+++ Components/Testimonial/Testimonial.js
@@ -1,30 +1,75 @@
-// import React from "react";
-import { Box } from "@chakra-ui/react";
-import Slider from "react-slick";
-import Image from "next/image";
-import nature19 from "../../public/images/nature19.jpg";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Card from "../../Components/Testimonial/Card";
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 
 const Testimonial = () => {
-  return (
-    <>
-      <Box
-        className="testimonial"
-        style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
-      >
-        <Box style={{ width: "50%" }}>
-          <h2>Testimonials</h2>
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
 
-          <Slider>
-            <Card img={nature19} alt="" />
-            <Card img="nature20" alt="" />
-            <Card img="nature17" alt="" />
-          </Slider>
-        </Box>
-      </Box>
-    </>
+  //* useEffect function to negative numbers.
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  //* useEffect for the auto slider.
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> TESTIMONIALS
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, title, quote, name } = person;
+
+          //* conditions for the slide.
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          //* mapping the json/data
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title} </p>
+              <p className="text">{quote} </p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   );
 };
 
