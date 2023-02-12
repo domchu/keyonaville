676d511ccaff5a09976ef7556d9723184b83ef4e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-12 12:58:59 +0100

    chores:added the unicode icons

--- Components/WhyKeyona.js
+++ Components/WhyKeyona.js
@@ -11,7 +11,7 @@ const WhyKeyona = () => {
 
         <Grid
           justifyContent="center"
-          // alignItems="center"
+          alignItems="center"
           // textAlign="center"
           GridTemplateColumns={{
             base: "1fr",
@@ -21,42 +21,59 @@ const WhyKeyona = () => {
           }}
         >
           <Box className="why-text">
-            <h4>Quality Learning</h4>
+            <h4>
+              Quality Learning <span style={{ fontSize: "2rem" }}>&#9758;</span>
+            </h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
           <Box className="why-text">
-            <h4>Problem Solvers</h4>
+            <h4>
+              Problem Solvers
+              <span style={{ fontSize: "1.7rem" }}>&#9836;</span>
+            </h4>
             <p>
               A school where our future generations are developed with a
               critical mind set to become problem solvers in our society.
             </p>
           </Box>
           <Box className="why-text">
-            <h4>Innovative Teaching</h4>
+            <h4>
+              Innovative Teaching
+              <span style={{ fontSize: "1.7rem" }}>&#9885;</span>
+            </h4>
             <p>
               A school where high innovative teaching and learning is given at
               an affordable tuition
             </p>
           </Box>
           <Box className="why-text">
-            <h4>Conducive Environment</h4>
+            <h4>
+              Conducive Environment
+              <span style={{ fontSize: "1.7rem" }}>&#9788;</span>
+            </h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
           <Box className="why-text">
-            <h4>Experience Teachers</h4>
+            <h4>
+              Experience Teachers
+              <span style={{ fontSize: "1.7rem" }}>&#9770;</span>
+            </h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
           <Box className="why-text">
-            <h4>Experte Methodology</h4>
+            <h4>
+              Experte Methodology
+              <span style={{ fontSize: "1.7rem" }}>&#9883;</span>
+            </h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
