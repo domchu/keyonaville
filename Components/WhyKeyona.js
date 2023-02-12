b1910e7f9d3e76e44deef7be9a23153bc560446c
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-12 12:39:22 +0100

    styles:styles the why keyona

--- Components/WhyKeyona.js
+++ Components/WhyKeyona.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Grid } from "@chakra-ui/react";
 
 const WhyKeyona = () => {
   return (
@@ -9,60 +9,60 @@ const WhyKeyona = () => {
           <h2>WHY KEYONAVILLE SCHOOL</h2>
         </Box>
 
-        <Flex
+        <Grid
           justifyContent="center"
-          alignItems="center"
+          // alignItems="center"
           // textAlign="center"
-          flexDirection={{
+          GridTemplateColumns={{
             base: "1fr",
             sm: "1fr",
             md: "1fr 1fr",
-            lg: "1fr 1fr 1fr",
+            lg: "repeat(3, 1fr)",
           }}
         >
-          <Box>
-            <h2>Quality Learning</h2>
+          <Box className="why-text">
+            <h4>Quality Learning</h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
-          <Box>
-            <h2>Problem Solvers</h2>
+          <Box className="why-text">
+            <h4>Problem Solvers</h4>
             <p>
               A school where our future generations are developed with a
               critical mind set to become problem solvers in our society.
             </p>
           </Box>
-          <Box>
-            <h2>Innovative Teaching</h2>
+          <Box className="why-text">
+            <h4>Innovative Teaching</h4>
             <p>
               A school where high innovative teaching and learning is given at
               an affordable tuition
             </p>
           </Box>
-          <Box>
-            <h2>Conducive Environment</h2>
+          <Box className="why-text">
+            <h4>Conducive Environment</h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
-          <Box>
-            <h2>Experience Teachers</h2>
+          <Box className="why-text">
+            <h4>Experience Teachers</h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
-          <Box>
-            <h2>Experte Methodology</h2>
+          <Box className="why-text">
+            <h4>Experte Methodology</h4>
             <p>
               A school where high moral and quality learning is built in the
               life of our future generations.
             </p>
           </Box>
-        </Flex>
+        </Grid>
       </Box>
     </>
   );
