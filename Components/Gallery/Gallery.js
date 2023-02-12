1c54cd6c97de48d748e6d08e5af90060669edb0e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-12 23:46:38 +0100

    chores:put all images in form of json

--- Components/Gallery/Gallery.js
+++ Components/Gallery/Gallery.js
@@ -1,5 +1,30 @@
 import React from "react";
 
+
+const images = [
+  { id: "1", imageName: "1.jpg", tag: "playground" },
+  { id: "2", imageName: "2.jpg", tag: "excursion" },
+  { id: "3", imageName: "3.jpg", tag: "ict" },
+  { id: "4", imageName: "4.jpg", tag: "library" },
+  { id: "5", imageName: "5.jpg", tag: "students" },
+  { id: "6", imageName: "5.jpg", tag: "events" },
+  { id: "7", imageName: "6.jpg", tag: "playground" },
+  { id: "8", imageName: "7.jpg", tag: "excursion" },
+  { id: "9", imageName: "8.jpg", tag: "ict" },
+  { id: "10", imageName: "9.jpg", tag: "library" },
+  { id: "11", imageName: "10.jpg", tag: "students" },
+  { id: "12", imageName: "11.jpg", tag: "events" },
+  { id: "13", imageName: "12.jpg", tag: "playground" },
+  { id: "14", imageName: "13.jpg", tag: "excursion" },
+  { id: "15", imageName: "14.jpg", tag: "ict" },
+  { id: "16", imageName: "15.jpg", tag: "library" },
+  { id: "17", imageName: "16.jpg", tag: "students" },
+  { id: "18", imageName: "17.jpg", tag: "events" },
+  { id: "19", imageName: "18.jpg", tag: "playground" },
+  { id: "20", imageName: "19.jpg", tag: "excursion" },
+  { id: "21", imageName: "20.jpg", tag: "ict" },
+];
+
 const Gallery = () => {
   return (
     <>
