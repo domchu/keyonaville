de19e245ae6dbfdd7ddf2c5199c478da29a4677b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-12 13:42:21 +0100

    chores:created the facilities componets

---
+++ Components/Facilities.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Facilities = () => {
+  return (
+    <>
+      <Box>
+        <Box className="title">
+          <h2>
+            <span>/</span>KEYONAVILLE FACILITIES
+          </h2>
+        </Box>
+      </Box>
+    </>
+  );
+};
+export default Facilities;
