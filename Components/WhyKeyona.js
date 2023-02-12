aeb0935d21548bd369f5f14552c7cf1281260e40
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-12 11:38:02 +0100

    chores: six reasons for choosing keyona

--- Components/WhyKeyona.js
+++ Components/WhyKeyona.js
@@ -4,23 +4,66 @@ import { Box, Flex } from "@chakra-ui/react";
 const WhyKeyona = () => {
   return (
     <>
-      <Flex
-        justifyContent="center"
-        flexDirection={{
-          base: "1fr",
-          sm: "1fr",
-          md: "1fr 1fr",
-          lg: "1fr 1fr 1fr",
-        }}
-      >
+      <Box>
         <Box>
-          <h2>Quality Learning</h2>
-          <p>
-            A school where high moral and quality learning is built in the life
-            of our future generations.
-          </p>
+          <h2>WHY KEYONAVILLE SCHOOL</h2>
         </Box>
-      </Flex>
+
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          // textAlign="center"
+          flexDirection={{
+            base: "1fr",
+            sm: "1fr",
+            md: "1fr 1fr",
+            lg: "1fr 1fr 1fr",
+          }}
+        >
+          <Box>
+            <h2>Quality Learning</h2>
+            <p>
+              A school where high moral and quality learning is built in the
+              life of our future generations.
+            </p>
+          </Box>
+          <Box>
+            <h2>Problem Solvers</h2>
+            <p>
+              A school where our future generations are developed with a
+              critical mind set to become problem solvers in our society.
+            </p>
+          </Box>
+          <Box>
+            <h2>Innovative Teaching</h2>
+            <p>
+              A school where high innovative teaching and learning is given at
+              an affordable tuition
+            </p>
+          </Box>
+          <Box>
+            <h2>Conducive Environment</h2>
+            <p>
+              A school where high moral and quality learning is built in the
+              life of our future generations.
+            </p>
+          </Box>
+          <Box>
+            <h2>Experience Teachers</h2>
+            <p>
+              A school where high moral and quality learning is built in the
+              life of our future generations.
+            </p>
+          </Box>
+          <Box>
+            <h2>Experte Methodology</h2>
+            <p>
+              A school where high moral and quality learning is built in the
+              life of our future generations.
+            </p>
+          </Box>
+        </Flex>
+      </Box>
     </>
   );
 };
