57ba2296c3899031f9b289d97f4ae712137daa56
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-13 23:16:32 +0100

    chores:make adjustment at  about & core value page

--- Components/CoreValue.js
+++ Components/CoreValue.js
@@ -16,57 +16,59 @@ const CoreValue = () => {
           </h2>
         </Box>
         <Flex className="core-container">
-          <Box className="core-text">
-            <Flex width="500px">
-              <Box>
-                <RiBookLine className="core-value-icons" />
-              </Box>
-              <Box className="core-value">
-                <h3>OUR Value</h3>
-                <li className="core-value-li">Capacity Building</li>
-                <li className="core-value-li">Punctuality</li>
-                <li className="core-value-li">Sacrifices</li>
-                <li className="core-value-li">Teamwork</li>
-                <li className="core-value-li">Neatness</li>
-                <li className="core-value-li">Integrity</li>
-                <li className="core-value-li">Possibility</li>
-              </Box>
-            </Flex>
-            <Flex justifyContent="space-around" mt={20} mb={20}>
-              <Box>
-                <GiFlowerPot className="core-value-icons" />
-              </Box>
-              <Box className="core-value">
-                <h3>OUR VISION</h3>
-                <p className="wrap-it">
-                  To raise boys and girls of character and excellence in wisdom
-                  and humility who shall contribute to their immediate society
-                  and nation building.
-                </p>
-              </Box>
-            </Flex>
-            <Flex justifyContent="space-around">
-              <Box>
-                <TfiPencil className="core-value-icons" />
-              </Box>
-              <Box className="core-value">
-                <h3>OUR MISSION</h3>
-                <p>
-                  To provide all round quality education of international
-                  standard to biys and girls with emphasis on character
-                  training, vocation and academic excellence
-                </p>
-              </Box>
-            </Flex>
-          </Box>
-          <Box>
-            <Image
-              src={students}
-              alt="Students"
-              height={600}
-              width={500}
-              className="students-img"
-            />
+          <Box className="flex-content">
+            <Box className="core-text">
+              <Flex width="">
+                <Box>
+                  <RiBookLine className="core-value-icons" />
+                </Box>
+                <Box className="core-value">
+                  <h3>OUR Value</h3>
+                  <li className="core-value-li">Capacity Building</li>
+                  <li className="core-value-li">Punctuality</li>
+                  <li className="core-value-li">Sacrifices</li>
+                  <li className="core-value-li">Teamwork</li>
+                  <li className="core-value-li">Neatness</li>
+                  <li className="core-value-li">Integrity</li>
+                  <li className="core-value-li">Possibility</li>
+                </Box>
+              </Flex>
+              <Flex justifyContent="space-around" mt={20} mb={20}>
+                <Box>
+                  <GiFlowerPot className="core-value-icons" />
+                </Box>
+                <Box className="core-value">
+                  <h3>OUR VISION</h3>
+                  <p className="wrap-it">
+                    To raise boys and girls of character and excellence in
+                    wisdom and humility who shall contribute to their immediate
+                    society and nation building.
+                  </p>
+                </Box>
+              </Flex>
+              <Flex justifyContent="space-around">
+                <Box>
+                  <TfiPencil className="core-value-icons" />
+                </Box>
+                <Box className="core-value">
+                  <h3>OUR MISSION</h3>
+                  <p>
+                    To provide all round quality education of international
+                    standard to biys and girls with emphasis on character
+                    training, vocation and academic excellence
+                  </p>
+                </Box>
+              </Flex>
+            </Box>
+            <Box>
+              <Image
+                src={students}
+                alt="Students"
+                height={600}
+                width={500}
+                className="students-img"
+              />
+            </Box>
           </Box>
         </Flex>
       </Box>
