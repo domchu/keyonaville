97383fb05ee8cedd74d78dddd46d96b32c250d83
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-13 23:58:13 +0100

    chores:created the topmenu & imorted it 2 the home

---
+++ Components/Topmenu/Topmenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Topmenu = () => {
+  return (
+    <>
+      <Box>
+        <Flex>
+          <Box></Box>
+        </Flex>
+        <Flex>
+          <Box></Box>
+        </Flex>
+      </Box>
+      <h4>top Menu</h4>
+    </>
+  );
+};
+
+export default Topmenu;
