7858263107bc201982e39cdce39509848ccd2ef8
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-16 23:43:24 +0100

    chore:style the 404 page

--- pages/404.js
+++ pages/404.js
@@ -26,14 +26,19 @@ const Error = () => {
       <Topmenu />
       <Navbar />
       <Box>
-        <Grid placeItems="center" h={200} textAlign="center" fontSize="2rem">
+        <Grid
+          placeItems="center"
+          h={200}
+          textAlign="center"
+          fontSize="2rem"
+          mt={30}
+          mb={30}
+        >
           <h2>Oooops...</h2>
           <h3>404 | That page could not be found.</h3>
           <p>
             Go back to the
-            <Link href="/">
-              Homepage
-            </Link>
+            <Link href="/">Home page</Link>
           </p>
         </Grid>
       </Box>
