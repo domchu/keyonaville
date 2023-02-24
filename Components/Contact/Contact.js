b782eee153a36ffc0e8ed50ebc466ec960217a0d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-24 23:18:27 +0100

    chores:added the form property

--- Components/Contact/Contact.js
+++ Components/Contact/Contact.js
@@ -8,8 +8,7 @@ import {
 } from "react-icons/sl";
 import Link from "next/link";
 import { FaFacebook, FaTwitter, FaInstagram, FaWhatsapp } from "react-icons/fa";
-
-
+import Form from "../Form";
 
 const Contact = () => {
   return (
@@ -64,64 +63,9 @@ const Contact = () => {
           </Box>
         </Grid>
       </Box>
-      <Box className="contact-form">
-        <div className="contact-text">
-          <div className="title">
-            <h2>
-              <span>/</span> WE'LL BE IN TOUCH SOON!
-            </h2>
-          </div>
-          <h2>Get in touch with us</h2>
-        </div>
-        <form action="" method="post">
-          <Box className="form">
-            <Box>
-              <label htmlFor="">
-                <input type="text" name="First Name" placeholder="First Name" />
-              </label>
-            </Box>
-            <Box>
-              <label htmlFor="">
-                <input type="text" name="First Name" placeholder="Last Name" />
-              </label>
-            </Box>
-            <Box>
-              <label htmlFor="">
-                <input
-                  type="text"
-                  name="First Name"
-                  placeholder="Mobile Number"
-                />
-              </label>
-            </Box>
-            <Box>
-              <label htmlFor="">
-                <input
-                  type="text"
-                  name="First Name"
-                  placeholder="Email Address"
-                />
-              </label>
-            </Box>
-          </Box>
-          <Box>
-            <textarea
-              name=""
-              id=""
-              cols="40"
-              rows="10"
-              className="textarea"
-              placeholder="Message"
-            ></textarea>
-          </Box>
-          <Box className="summit__btn__container">
-            <button type="submit" className="summit__btn">
-              SEND MESSAGE
-            </button>
-          </Box>
-        </form>
-        {/* find us on social media */}
-      </Box>
+      {/* form  */}
+      <Form />
+      {/* find us on social media */}
       <div className="title">
         <h2>
           <span>/</span> #FOLLOW US ON SOCIAL MEDIA
--- Components/Form.js
+++ Components/Form.js
@@ -1,11 +1,68 @@
 import React from 'react'
+import { Box } from "@chakra-ui/react";
 
 const Form = () => {
   return (
-    <div>
-      <h2>Form</h2>
-    </div>
-  )
-}
+    <>
+      <Box className="contact-form">
+        <div className="contact-text">
+          <div className="title">
+            <h2>
+              <span>/</span> WE'LL BE IN TOUCH SOON!
+            </h2>
+          </div>
+          <h2>Get in touch with us</h2>
+        </div>
+        <form action="" method="post">
+          <Box className="form">
+            <Box>
+              <label htmlFor="">
+                <input type="text" name="First Name" placeholder="First Name" />
+              </label>
+            </Box>
+            <Box>
+              <label htmlFor="">
+                <input type="text" name="First Name" placeholder="Last Name" />
+              </label>
+            </Box>
+            <Box>
+              <label htmlFor="">
+                <input
+                  type="text"
+                  name="First Name"
+                  placeholder="Mobile Number"
+                />
+              </label>
+            </Box>
+            <Box>
+              <label htmlFor="">
+                <input
+                  type="text"
+                  name="First Name"
+                  placeholder="Email Address"
+                />
+              </label>
+            </Box>
+          </Box>
+          <Box>
+            <textarea
+              name=""
+              id=""
+              cols="40"
+              rows="10"
+              className="textarea"
+              placeholder="Message"
+            ></textarea>
+          </Box>
+          <Box className="summit__btn__container">
+            <button type="submit" className="summit__btn">
+              SEND MESSAGE
+            </button>
+          </Box>
+        </form>
+      </Box>
+    </>
+  );
+};
 
 export default Form
