64ea2ccc5822ab94655f2476d106a28beb4ddbd4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-22 20:07:23 +0100

    chore:install next seo mudule

---
+++ next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.keyonavilleschools.com.ng",
+    siteName: "keyonaville school",
+    images: [
+      {
+        url: "https://www.keyonavilleschools.com.ng/favblack.png",
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
